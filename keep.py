d570af1de1c1a2dd2f2ff1e461446351b9b43800
lizytalk <lizytalk@outlook.com>
2019-04-13 20:18:15 +0800

    update

--- keep.py
+++ keep.py
@@ -3,9 +3,7 @@ import datetime
 import os
 import subprocess
 while True:
-    print(datetime.datetime.now()) 
-    task = subprocess.Popen(r"bash -c 'cd /Users/lizytalk/Projects/talk/ && (hexo generate \|\& tee -a hexo.log)'", shell=True)
-    task.wait()
-    task = subprocess.Popen(r"bash -c 'cd /Users/lizytalk/Projects/talk/ && (hexo deploy   \|\& tee -a hexo.log)'", shell=True)
+    print(datetime.datetime.now())
+    task = subprocess.Popen(r"bash -c 'cd /Users/lizytalk/Projects/talk/ && (git add source && git commit -m update&& git push   \|\& tee -a hexo.log)'", shell=True)
     task.wait()
     time.sleep(60)
