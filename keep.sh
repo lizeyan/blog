6675c05ed7de735cfc471ed32d836b858192a49a
lizytalk <lizytalk@outlook.com>
2019-04-19 08:51:40 +0800

    keep

--- keep.sh
+++ keep.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
+git pull
 git add source
 git commit -m "auto commit at $(date)"
 git push origin master
