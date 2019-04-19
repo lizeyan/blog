96276c18f9be82a130c15f9fab75983d2c198731
lizeyan <zy-li14@outlook.com>
2019-04-19 14:40:27 +0800

    update

--- keep.sh
+++ keep.sh
@@ -2,6 +2,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 git pull origin master
-git add source
-git commit -m "auto commit at $(date)"
-git push origin master
+cd /git/directory
+if [[ `git status --porcelain` ]]; then
+  # Changes
+  git add source
+  git commit -m "auto commit at $(date)"
+  git push origin master
+else
+  # No changes
+  echo No changes
+fi
